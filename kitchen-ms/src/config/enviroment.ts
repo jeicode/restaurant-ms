ba17bfbbd604eccfe7c53fb6bea2a5f69e7d6dfd
Jeison Obando <jeison.obando@paxzu.co>
2025-04-10 15:35:00 -0500

    config kitchen ms

--- kitchen-ms/src/config/enviroment.ts
+++ kitchen-ms/src/config/enviroment.ts
@@ -2,6 +2,5 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const environment = {
-    PORT: get('PORT').required().asPortNumber(),
     RABBITMQ_URL: get('RABBITMQ_URL').required().asString()
 }
